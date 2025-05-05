fa981e0011b0d6a19e6e932234968185f17c0601
anaxositashvili <xositashvili2012@gmail.com>
2025-05-05 22:23:37 +0400

    GOA

---
+++ day 3/homework/homework.py
@@ -0,0 +1,16 @@
+name="ana"
+surename="khositashvili"
+age="13"
+
+print(name+ " " +surename+ " " +str(age))
+
+# 3) ეცადეთ, რომ ლამაზად გამოიტანოთ ტერმინალში, მაგალითად: (Hello, my name is Andria and my surname is lezhava, I am 16 years old).
+print ( 'Hello, my name is' + name + ' and my surname is ' + surename + ' I am  ' + str(age) + ' years old ')
+
+# 4) შექმენით 5 ცვლადი სადაც შეინახავთ რიცხვებს, მაგალითად: (15, 432, 25, 3, 45, 346) ამ ცვლადებზე უნდა მოახდინოთ ოპერაციები, მაგალითად მიმატება, გამოკლება, გამრავლება და გაყოფ
+print(15+45)
+print(432/2)
+print(25*2)
+print(3/3)
+print(45-23)
+print(346-26)
---
+++ day 4/classwork/classwork.py
@@ -0,0 +1,6 @@
+name="ana"
+surename="khositashvili"
+age=13
+
+
+print=('Hello, my name is' , name , ' and my surname is ' , surename , ' I am  ' ,str(age) , ' years old ' )
