24b4e83cb0d8a8d32c45004aaa29d24bad266aec
anaxositashvili <xositashvili2012@gmail.com>
2025-05-08 21:24:57 +0400

    GOA

---
+++ day 4/homework/homework.py
@@ -0,0 +1,30 @@
+#1) მომხარებელს შემოატანინეთ სახელი და ასაკი და დაპრინტეთ შემდეგ ფორმატში: "Name: სახელი, Age: ასაკი" name=input("enter your name: ")
+name=input("enter your name: ")
+print(name)
+age=int(input("enter you age: "))
+print(age)
+#2) მომხმარებელს შემოატანინეთ ორი რიცხვითი მნიშვნელობა (სიგანე და სიმაღლე). ტერმინალში გამოიტანეთ ფართობი/პერიმეტრი. S = ფართობი (S = width*height)/P = პერიმეტრი (P = (width+height)*2
+width=int(input("enter width: "))
+height=int(input("enter height: "))
+
+print((width+height)*2)
+print(width*height)
+
+
+
+#3) კომენტარების სახით ახსენი რა არის input და output
+
+#input-ნებისმიერი რამ რაც კომპიუტერში შედის
+#output-ნებისმიერი რამ რაც კომპიუტერიდან გამოდის
+
+
+
+#4) შემოატანინეთ მომხმარებელს ორი რიცხვი და დაპრინტეთ მათი ჯამი, სხვაობა, ნამრავლი და განაყოფი.
+
+num=int(input("enter random number: "))
+num1=int(input("enter random number: "))
+
+print(num-num1)
+print(num/num1)
+print(num1+num)
+print(num1*num)
