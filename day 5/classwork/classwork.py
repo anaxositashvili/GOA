6e1f3c3305badf37c4001f8df656f55bdac3d67a
anaxositashvili <xositashvili2012@gmail.com>
2025-05-11 23:40:54 +0400

    GOA

--- day 5/classwork/classwork.py
+++ day 5/classwork/classwork.py
@@ -1,5 +1,1 @@
-first_name=input("enter your name: ")
-last_name=input("enter your lastname: ")
-
-full_name=first_name + " " + last_name
-print("full_name: ")
+print("hello"[1])
---
+++ day 5/homework/homework.py
@@ -0,0 +1,39 @@
+#1)შემოატანინეთ მომხმარებელს რიცხვი  და თუ 10 ზე მეტი იქნება  გამოიტანინოს ("you are right ")
+a=6
+b=5
+answer=a + b
+if answer > 10:
+    print("you are right")
+    
+#2)მომხმარებელს შემოატანინეთ ორი რიცხვი გააკეთეთ მათზე მათემატიკური ოპერაციები  ("+, -, *, /, %, <, >, <=, >=, ==, !=. **) 
+num=27
+num1=2
+
+print(num+num1)
+print(num-num1)
+print(num1*num)
+print(num/num1)
+print(num%num1)
+print(num>num1)
+print(num1<num)
+print(num1!=num)
+
+
+
+#3)ახსენით დღეს ნასწავლი If კომენტარებით  
+if True:
+    print("good morning")
+    "good evening"
+
+#ამ შემთხვევაში თუ დავწერთ if-ის  მერე  true   გამოიტანს სწორს ხოლო თუ დავწერთ  false გამოიტანს არასწორს
+
+
+    
+
+  
+
+
+
+
+
+
